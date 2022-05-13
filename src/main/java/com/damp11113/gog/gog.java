90a10002ca7103ebfeed969af2224d4c006a02fd
damp11113 <64675096+damp11113@users.noreply.github.com>
2022-05-13 22:11:09 +0700

    plugin

---
+++ src/main/java/com/damp11113/gog/gog.java
@@ -0,0 +1,17 @@
+package com.damp11113.gog;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class gog extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
